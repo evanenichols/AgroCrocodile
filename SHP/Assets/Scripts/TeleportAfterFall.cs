53d124ae028180ac475fd4662311f0bbe6ed6113
Unknown <evan.e.nichols@gmail.com>
2017-09-13 09:53:46 -0700

    initial submission
    after working for a couple days without source control, this is my first submission

---
+++ SHP/Assets/Scripts/TeleportAfterFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportAfterFall : MonoBehaviour {
+
+	Vector3 startLocation;
+
+
+	// Use this for initialization
+	void Start () {
+		startLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -20) {
+			transform.position = startLocation;
+		}
+	}
+}
