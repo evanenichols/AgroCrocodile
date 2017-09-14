9c45831e9fb858f4f1ed5ca93d347d080f36ddb2
Unknown <evan.e.nichols@gmail.com>
2017-09-14 12:32:42 -0700

    Added the boxhouse Scene, and a scene changer

---
+++ SHP/Assets/Scripts/SceneChangeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeTrigger : MonoBehaviour {
+	public string nextScene;
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			SceneManager.LoadScene (nextScene);
+		}
+	}
+}
