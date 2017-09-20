c2ffee1af519ef91a19856914d2f61f10f5d8d62
Unknown <evan.e.nichols@gmail.com>
2017-09-20 14:14:48 -0700

    Added vectors to help visualize quaternions

--- SHP/Assets/Testing/ThrowBallScript.cs
+++ SHP/Assets/Testing/ThrowBallScript.cs
@@ -7,19 +7,52 @@ public class ThrowBallScript : MonoBehaviour {
 	public GameObject thingToCreate;
 	public float initialSpeed;
 	public float angleFromHorizontal;
-	GameObject item;
+	List<GameObject> items = new List<GameObject>();
+
+	void Start()
+	{
+		Vector3 start = new Vector3 (0, 0, 0), end = new Vector3 (0, 1, 0);
+		GameObject upLine = null;
+		Lines.MakeArrow (ref upLine, start, end, 2, Color.red);
+		GameObject arcLine = null;
+		Vector3 dir = (end - start).normalized;
+		Lines.MakeArcArrow(ref arcLine, 270, 32, 2, dir, start+new Vector3 (0,0,0.5f), 
+			new Vector3(0,0,0), Color.red);
+	}
+
+	public void Update () 
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			ThrowItem();
+		}
+	}
 
 	public void ThrowItem() {
 		Vector3 p = gameObject.transform.position+gameObject.transform.forward;
-		item = Instantiate (thingToCreate, p, gameObject.transform.rotation);
+		Debug.Log (gameObject+" wants to shoot a "+thingToCreate);
+		GameObject item = Instantiate (thingToCreate, p, gameObject.transform.rotation);
 		Quaternion q = item.transform.rotation;
 		Quaternion r = Quaternion.Euler (-angleFromHorizontal, 0, 0);
+		GameObject qAxis = null, qArc = null;
+		float angle;
+		Vector3 axis;
+		Debug.Log (q);
+		r.ToAngleAxis (out angle, out axis);
+		Lines.MakeArrow (ref qAxis, transform.position, transform.position + q*axis);
+		Lines.MakeArcArrow (ref qArc, angle, 32, 4, q*axis, 
+						transform.forward, transform.position);
+
+
 		item.transform.rotation = q * r;
 		Rigidbody rb = item.GetComponent<Rigidbody> ();
 		rb.velocity = item.transform.forward * initialSpeed;
+		items.Add (item);
 	}
 
-	public void DeleteItem() {
-		Destroy (item);
+	public void DestroyALl() {
+		for (int i = 0; i < items.Count; i++) {
+			Destroy (items [i]);
+		}
+		items.Clear ();
 	}
 }
