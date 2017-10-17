1086518c2a68162ef9a50a1bbda3d6e1af9cbb7c
Unknown <evan.e.nichols@gmail.com>
2017-10-17 08:25:02 -0700

    Linked Scoreboard to ScoringTriggers
    *3D TextMeshPro objects now keep track of the score

--- SHP/Assets/Scripts/DidScoreTrigger.cs
+++ SHP/Assets/Scripts/DidScoreTrigger.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class DidScoreTrigger : MonoBehaviour {
 
+	public TMPro.TextMeshPro output;
+	public string HomeGuest;
+	private int score = 0;
+
+	void Start() {
+		output.text = HomeGuest + " :" + (score);
+	}
+
 	void OnTriggerEnter(Collider col) {
-		Debug.Log ("You Scored!");
+		Debug.Log (HomeGuest + " Scored!");
+		output.text = HomeGuest + " :" + (++score);
 	}
 }
